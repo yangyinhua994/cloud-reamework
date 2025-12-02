53bdaea5c267b0575079dccfbec553f4b39cbca2
yangyinhua <1714564639@qq.com>
2025-12-02 14:58:35 +0800

    心中order服务

--- base-service/src/main/java/com/example/controller/PickupAddressController.java
+++ base-service/src/main/java/com/example/controller/PickupAddressController.java
@@ -1,16 +1,10 @@
 package com.example.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.convert.PickupAddressConvert;
 import com.example.dto.PickupAddressDTO;
 import com.example.entity.PickupAddress;
-import com.example.groups.Add;
-import com.example.groups.Update;
-import com.example.response.Response;
 import com.example.service.PickupAddressService;
 import com.example.vo.PickupAddressVO;
-import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
